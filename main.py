b93f9034d5fb23e3a4edba6f1bfe57873dc595f3
Alias Kevin Kunju <alias.kevin@ymail.com>
2023-06-23 23:26:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from question_model import Question
 from data import question_data
 from quiz_brain import QuizBrain
 from ui import QuizInterface
-
+# Creating a list of Question objects with attributes
 question_bank = []
 for question in question_data:
     question_text = question["question"]
@@ -10,12 +10,12 @@ for question in question_data:
     new_question = Question(question_text, question_answer)
     question_bank.append(new_question)
 
-
+# Creating Quizbrain object and Quiz UI object
 quiz = QuizBrain(question_bank)
 quiz_ui = QuizInterface(quiz)
 
 # while quiz.still_has_questions():
 #     quiz.next_question()
-
+# Showing score after Quiz completion
 print("You've completed the quiz")
 print(f"Your final score was: {quiz.score}/{quiz.question_number}")
